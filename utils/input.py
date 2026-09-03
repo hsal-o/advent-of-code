6376da46b73a1c3ccc6fb4e21a52704bf53035de
Hector Salinas <hmsalinas31@outlook.com>
2026-09-03 09:51:09 -0500

    fix: strip lines in until.input.get_lines

--- utils/input.py
+++ utils/input.py
@@ -2,7 +2,7 @@ def get_lines(file_name):
     try:
         with open(file_name, 'r') as file:
             lines = file.readlines()
-            return lines
+            return [line.strip() for line in lines]
     except Exception:
         print(f"File '{file_name}' not found")
         return None
