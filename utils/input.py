609fc0dddcbddc3683a7649e44774c07548a2efe
Yumba <hmsalinas31@outlook.com>
2026-09-09 20:38:23 -0500

    refactor: update utils.input to add optional strip flag

--- utils/input.py
+++ utils/input.py
@@ -1,8 +1,12 @@
-def get_lines(file_name):
+def get_lines(file_name, strip=True):
     try:
         with open(file_name, 'r') as file:
             lines = file.readlines()
-            return [line.strip() for line in lines]
+
+            if(strip):
+                return [line.strip() for line in lines]
+            else:
+                return [line.rstrip("\n") for line in lines]
     except Exception:
         print(f"File '{file_name}' not found")
         return None
